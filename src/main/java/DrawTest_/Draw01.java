7f21f9f7b8a26d71330462f1e19a8a7d91ea86a3
Osamazzz <1176091035@qq.com>
2022-03-03 21:28:27 +0800

    second

---
+++ src/main/java/DrawTest_/Draw01.java
@@ -0,0 +1,11 @@
+package DrawTest_;
+
+/**
+ * @author Obamazzz
+ * @version 1.0
+ */
+public class Draw01 {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
